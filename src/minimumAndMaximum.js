bb00a45a3fd924471b7289f8419f1b97d62bcff7
Will Williams <wwilliams@indeed.com>
2024-02-27 11:17:07 -0800

    Implements minimumAndMaximumIterate

--- src/minimumAndMaximum.js
+++ src/minimumAndMaximum.js
@@ -18,6 +18,23 @@ function minimumAndMaximumSort(numbers) {
 /**
  * Implement an algorithm that uses iteration to find the minimum and maximum
  */
-function minimumAndMaximumIterate(numbers) {}
+function minimumAndMaximumIterate(numbers) {
+  if (numbers.length >= 1) {
+    let min = numbers[0];
+    let max = numbers[0];
+    // Iterate through each number in the array
+    numbers.forEach((number) => {
+      if (number < min) {
+        min = number;
+      }
+      // Update max if the current number is larger
+      if (number > max) {
+        max = number;
+      }
+    });
+    return [min, max];
+  }
+  return [];
+}
 
 module.exports = { minimumAndMaximumIterate, minimumAndMaximumSort };
