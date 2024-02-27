7d03934b92b1efec2da4cb049d955a8fd8bd254a
Will Williams <wwilliams@indeed.com>
2024-02-27 11:16:29 -0800

    Implements minimumAndMaximumSort

--- src/minimumAndMaximum.js
+++ src/minimumAndMaximum.js
@@ -1,7 +1,19 @@
 /**
  * Implement an algorithm that sorts the array then returns the minimum and maximum
  */
-function minimumAndMaximumSort(numbers) {}
+function minimumAndMaximumSort(numbers) {
+  const minMax = [];
+
+  // Check if the array has at least one element
+  if (numbers.length >= 1) {
+    // Sort in ascending order
+
+    numbers.sort((a, b) => a - b);
+    minMax.push(numbers[0]);
+    minMax.push(numbers[numbers.length - 1]);
+  }
+  return minMax;
+}
 
 /**
  * Implement an algorithm that uses iteration to find the minimum and maximum
