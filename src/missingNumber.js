2ac4251e6b21ae703bfaeb163ff5a34056036637
Will Williams <wwilliams@indeed.com>
2024-02-27 11:13:05 -0800

    Implements missingNumberBruteForce

--- src/missingNumber.js
+++ src/missingNumber.js
@@ -1,7 +1,20 @@
 /**
  * Implement a brute force algorithm for finding the missing number in an array
  */
-function missingNumberBruteForce(numbers) {}
+function missingNumberBruteForce(numbers) {
+  for (let i = 1; i <= numbers.length + 1; i++) {
+    let match = false;
+    for (let j = 0; j < numbers.length; j++) {
+      if (numbers[j] === i) {
+        match = true;
+        break;
+      }
+    }
+    if (!match) {
+      return i;
+    }
+  }
+}
 
 /**
  * Use an iterative  strategy for finding the missing number in an array
